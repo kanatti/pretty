296816dfed4072cc7d1287c4e5e9321c6c57dbab
Balagopal Kanattil <balukanattil@gmail.com>
2023-01-03 01:04:24 -0500

    Basic table rendering for JSON

---
+++ src/draw.rs
@@ -0,0 +1,144 @@
+use std::iter;
+
+const LEFT_TOP: char = '┌';
+const RIGHT_TOP: char = '┐';
+const LEFT_BOTTOM: char = '└';
+const RIGHT_BOTTOM: char = '┘';
+const HORIZONTAL: char = '─';
+const HORIZONTAL_DOWN: char = '┬';
+const HORIZONTAL_UP: char = '┴';
+const CROSS: char = '┼';
+const VERTICAL: char = '│';
+const VERTICAL_LEFT: char = '┤';
+const VERTICAL_RIGHT: char = '├';
+const SPACE: char = ' ';
+const NEW_LINE: char = '\n';
+
+#[derive(Debug)]
+pub struct Header {
+    pub name: String,
+    pub max_width: usize,
+}
+
+pub fn draw_box(height: usize, width: usize) -> String {
+    let mut chars = row(width, LEFT_TOP, HORIZONTAL, RIGHT_TOP);
+    chars.push(NEW_LINE);
+
+    for _ in 1..height - 2 {
+        chars.append(&mut row(width, VERTICAL, SPACE, VERTICAL));
+        chars.push(NEW_LINE);
+    }
+
+    chars.append(&mut row(width, LEFT_BOTTOM, HORIZONTAL, RIGHT_BOTTOM));
+
+    chars.iter().collect()
+}
+
+// TODO: Optimize later
+pub fn draw_table(headers: &[Header], rows: &Vec<Vec<String>>) -> String {
+    let row_count = rows.len() + 1;
+    let column_count = headers.len();
+
+    let mut table = String::from(LEFT_TOP);
+
+    // Top
+    for header in headers[..headers.len() - 1].iter() {
+        table.push_str(
+            &(0..header.max_width)
+                .map(|_| HORIZONTAL)
+                .chain(iter::once(HORIZONTAL_DOWN))
+                .collect::<String>(),
+        )
+    }
+
+    table.push_str(
+        &(0..headers.last().unwrap().max_width)
+            .map(|_| HORIZONTAL)
+            .chain(iter::once(RIGHT_TOP))
+            .collect::<String>(),
+    );
+
+    table.push(NEW_LINE);
+
+    // Headers
+
+    table.push(VERTICAL);
+
+
+    for header in headers.iter() {
+        table.push_str(&pad_left(&header.name, header.max_width));
+        table.push(VERTICAL)
+    }
+
+    table.push(NEW_LINE);
+
+    // Rows
+
+    for row in rows.iter() {
+        // Seperator
+
+        table.push(VERTICAL_RIGHT);
+
+        for header in headers[..headers.len() - 1].iter() {
+            table.push_str(
+                &(0..header.max_width)
+                    .map(|_| HORIZONTAL)
+                    .chain(iter::once(CROSS))
+                    .collect::<String>(),
+            )
+        }
+    
+        table.push_str(
+            &(0..headers.last().unwrap().max_width)
+                .map(|_| HORIZONTAL)
+                .chain(iter::once(VERTICAL_LEFT))
+                .collect::<String>(),
+        );
+
+        // Row content
+        table.push(NEW_LINE);
+
+        table.push(VERTICAL);
+
+        for (i, header) in headers.iter().enumerate() {
+            table.push_str(&pad_left(&row[i], header.max_width));
+            table.push(VERTICAL)
+        }
+    
+        table.push(NEW_LINE);
+    }
+
+    // Bottom
+    table.push(LEFT_BOTTOM);
+
+    for header in headers[..headers.len() - 1].iter() {
+        table.push_str(
+            &(0..header.max_width)
+                .map(|_| HORIZONTAL)
+                .chain(iter::once(HORIZONTAL_UP))
+                .collect::<String>(),
+        )
+    }
+
+    table.push_str(
+        &(0..headers.last().unwrap().max_width)
+            .map(|_| HORIZONTAL)
+            .chain(iter::once(RIGHT_BOTTOM))
+            .collect::<String>(),
+    );
+
+    table.push(NEW_LINE);
+
+    table
+}
+
+fn pad_left(s: &str, width: usize) -> String {
+    format!("{:<width$}", s, width=width)
+}
+
+fn row(width: usize, start: char, mid: char, end: char) -> Vec<char> {
+    iter::once(start)
+        .chain((0..width - 2).map(move |_| mid))
+        .chain(iter::once(end))
+        .collect()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-use std::fs;
+use std::{cmp, collections::HashMap, fs};
 
 use serde_json::{Result, Value};
 
 pub mod args;
+pub mod draw;
+
+use draw::Header;
 
 pub fn run(args: args::Args) {
     let data = fs::read_to_string(args.file_name).unwrap();
@@ -11,10 +14,101 @@ pub fn run(args: args::Args) {
 
     match value {
         Ok(val) => {
-            println!("{}", serde_json::to_string_pretty(&val).unwrap());
+            render(val);
         }
         Err(e) => {
             println!("{:#?}", e);
         }
+    }
+}
+
+fn render(value: Value) {
+    match value {
+        Value::Array(values) => {
+            render_table(&values);
+        }
+        Value::Object(mapped_values) => println!("Will render object"),
+        _ => println!("Unexpected path"),
+    }
+}
+
+fn render_table(values: &Vec<Value>) {
+    // Get Headers
+    let headers = get_headers(values);
+    let rows: Vec<Vec<String>> = values
+        .iter()
+        .map(|value| value_to_vec(value, &headers))
+        .collect();
+
+    println!("{}", draw::draw_table(&headers, &rows));
+}
+
+fn get_headers(values: &Vec<Value>) -> Vec<Header> {
+    let mut seen: HashMap<&str, usize> = HashMap::new();
+    let mut headers: Vec<String> = Vec::new();
+
+    values.iter().for_each(|value| match value {
+        Value::Object(map) => {
+            for (key, value) in map.iter() {
+                match seen.get(key as &str) {
+                    Some(max_width) => {
+                        seen.insert(key, cmp::max(*max_width, len(value)));
+                    }
+                    None => {
+                        seen.insert(key, cmp::max(key.len(), len(value)));
+                        headers.push(String::from(key));
+                    }
+                }
+            }
+        }
+        _ => println!("Not implemented"),
+    });
+
+    headers
+        .into_iter()
+        .map(|name| Header {
+            max_width: *seen.get(&name as &str).unwrap_or(&0),
+            name,
+        })
+        .collect()
+}
+
+fn len(value: &Value) -> usize {
+    match value {
+        Value::Null => 4,    // null
+        Value::Bool(_) => 5, // true or false
+        Value::Number(n) => n.to_string().len(),
+        Value::String(s) => s.len() + 2,
+        Value::Array(_) => 4,  // [..]
+        Value::Object(_) => 4, // {..}
+    }
+}
+
+fn value_to_vec(value: &Value, headers: &Vec<Header>) -> Vec<String> {
+    match value {
+        Value::Object(object) => headers
+            .iter()
+            .map(|header| {
+                object
+                    .get(&header.name)
+                    .map(|val| to_string(val))
+                    .unwrap_or(String::from(""))
+            })
+            .collect(),
+        _ => vec![],
+    }
+}
+
+fn to_string(value: &Value) -> String {
+    match value {
+        Value::Null => String::from("null"),
+        Value::Bool(bool) => match bool {
+            true => String::from("true"),
+            false => String::from("false"),
+        },
+        Value::Number(n) => n.to_string(),
+        Value::String(s) => format!("\"{}\"", s),
+        Value::Array(_) => String::from("[..]"),
+        Value::Object(_) => String::from("{..}"),
     }
 }
